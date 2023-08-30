1f026d8906b3d265b4c53fb45f5073a83c297ea7
Cyrus Yiu <UnsignedArduino@outlook.com>
2023-08-29 22:59:26 -0400

    Use Vercel DB instead of Replit

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  rewrites: async () => {
-    return [
-      {
-        source: "/api",
-        destination:
-          "https://awesome-arcade-extensions-website-clicks-api-replit.ckyiu.repl.co/all",
-      },
-      {
-        source: "/api/:path*",
-        destination:
-          "https://awesome-arcade-extensions-website-clicks-api-replit.ckyiu.repl.co/:path*",
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
---
+++ src/pages/api/extensions/click.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { sql } from "@vercel/postgres";
+
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  try {
+    const repo = request.query.repo as string;
+    if (!repo) {
+      return response.status(404).json({ error: "Repository is required!" });
+    }
+    const result =
+      await sql`SELECT Repository, Clicks FROM ExtensionClicks WHERE Repository=${repo}`;
+    if (result.rows.length === 1) {
+      const row = result.rows[0];
+      const newClickCount = row["clicks"] + 1;
+      await sql`UPDATE ExtensionClicks SET Clicks=${newClickCount} WHERE Repository=${repo}`;
+      const responseJson: { [repo: string]: number } = {};
+      responseJson[repo] = newClickCount;
+      return response.status(200).json(responseJson);
+    } else {
+      return response.status(404).json({ error: "Repository does not exist!" });
+    }
+  } catch (error) {
+    return response.status(500).json({ error });
+  }
+}
