4ae08fe50e9e6be3830c28a4976759698f50a633
Cyrus Yiu <UnsignedArduino@outlook.com>
2023-09-10 20:27:24 -0400

    Delete embedded tools

--- src/scripts/embeddedTools/UnsignedArduino/Arcade-MIDI-to-Song-Online/midiToSong.ts
+++
@@ -1,8 +0,0 @@
-import { AnyEvent } from "midifile-ts";
-import { pxt } from "@/scripts/embeddedTools/UnsignedArduino/Arcade-MIDI-to-Song-Online/arcade/music";
-
-export default function midiToSong(
-  msgs: AnyEvent[],
-  trackToUse: string | number,
-  divisor: number
-): pxt.assets.music.Song {}
