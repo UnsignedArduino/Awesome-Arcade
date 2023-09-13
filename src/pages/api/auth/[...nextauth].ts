cf3dc987fed4dd5faae0d254e270dcc66dc668c7
Cyrus Yiu <UnsignedArduino@outlook.com>
2023-09-12 20:29:36 -0400

    Store all signed-in users in database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import queryDb from "@/scripts/Database";
 
 export const authOptions = {
   providers: [
@@ -10,6 +11,29 @@ export const authOptions = {
   ],
   pages: {
     error: "/auth/error", // Error code passed in query string as ?error=
+  },
+  callbacks: {
+    // @ts-ignore
+    async signIn({ user }) {
+      try {
+        const id = parseInt(user.id);
+        const { name, email, image } = user;
+        const matchingIDs = await queryDb(
+          "SELECT id FROM users WHERE EXISTS (SELECT id FROM users WHERE id=$1)",
+          [id]
+        );
+        if (matchingIDs.rowCount === 0) {
+          await queryDb(
+            "INSERT INTO users (id, name, email, profile) VALUES ($1, $2, $3, $4)",
+            [id, name, email, image]
+          );
+        }
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
   },
 };
 
