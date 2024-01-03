814ffcd7cd3501862c74ba784f9babc44a9b1c84
Cyrus Yiu <UnsignedArduino@outlook.com>
2024-01-03 18:20:37 -0500

    Add extension https://github.com/riknoll/arcade-screen-transitions and fix build error
    Fixes https://github.com/UnsignedArduino/Awesome-Arcade/issues/50

--- src/scripts/Utils/ParseOldExtensionsXML/index.ts
+++ src/scripts/Utils/ParseOldExtensionsXML/index.ts
@@ -1,9 +1,9 @@
 export { default } from "./parse";
+export type { ExtensionList } from "./parse";
 export type {
-  ExtensionList,
   Extension,
   Tool,
   ExtensionRef,
   ToolRef,
   URLLink,
-} from "./parse";
+} from "../ParseListXML/types";
