a78edac706fcbe109cbae59f6f54d0ad8a479f7e
Cyrus Yiu <UnsignedArduino@outlook.com>
2024-01-03 00:21:12 -0500

    Okay home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,6 +13,30 @@ type HomeProps = { appProps: AppProps };
 export function Home({ appProps }: HomeProps): JSX.Element {
   const { data: session } = useSession();
 
+  type QuickLinkPage = {
+    name: string;
+    description: string;
+    link: string;
+    linkText: string;
+  };
+
+  const quickLinkPages: QuickLinkPage[] = [
+    {
+      name: "Extensions",
+      description:
+        "A list of awesome MakeCode Arcade extensions to further your games!",
+      link: "/extensions",
+      linkText: "View awesome extensions",
+    },
+    {
+      name: "Tools",
+      description:
+        "A list of awesome MakeCode Arcade tools to help you develop great games!",
+      link: "/tools",
+      linkText: "View awesome tools",
+    },
+  ];
+
   return (
     <Layout
       title={pageName}
@@ -29,6 +53,43 @@ export function Home({ appProps }: HomeProps): JSX.Element {
         This is a website of awesome MakeCode Arcade extensions and tools that I
         find super useful (or just plain cool) in my projects.
       </p>
+      <div style={{ overflowX: "hidden" }}>
+        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-2 row-cols-xl-2">
+          {quickLinkPages.map((feature: QuickLinkPage, index: number) => {
+            return (
+              <div className="col mb-3 mt-1" key={`help-card-${index}`}>
+                <div className="card mb-2 h-100">
+                  {/* <Image
+                    src={feature.image}
+                    alt={feature.altText}
+                    className="card-img-top"
+                    objectFit="cover"
+                  /> */}
+                  <h5 className="card-title m-3 mb-0">{feature.name}</h5>
+                  <div className="card-body">
+                    <div className="card-text">
+                      <p>{feature.description}</p>
+                    </div>
+                    <Link
+                      href={
+                        feature.link.startsWith("/")
+                          ? feature.link
+                          : `/help/${feature.link}`
+                      }
+                      passHref
+                      legacyBehavior
+                    >
+                      <a className="card-link stretched-link">
+                        {feature.linkText}
+                      </a>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
       <p>
         Please note that this website is not developed, affiliated, or endorsed
         by Microsoft, the owner of MakeCode Arcade.
