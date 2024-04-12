293cae2ba29bb57f4efc23d17dde9c08c5d7e1b8
Cyrus Yiu <UnsignedArduino@outlook.com>
2024-04-12 17:10:52 -0400

    Update config for extensions and tools (do not deploy)

--- tina/config.ts
+++ tina/config.ts
@@ -25,6 +25,194 @@ export default defineConfig({
   schema: {
     collections: [
       {
+        name: "extensions",
+        label: "Extensions",
+        path: "content/extensions",
+        fields: [
+          {
+            type: "string",
+            name: "repo",
+            label: "GitHub repo URL",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "boolean",
+            name: "isJavascriptOnly",
+            label: "Is JavaScript only extension",
+          },
+          {
+            type: "rich-text",
+            name: "description",
+            label: "Description",
+            required: true,
+            isBody: true,
+          },
+          {
+            label: "Links",
+            name: "links",
+            type: "object",
+            list: true,
+            ui: {
+              itemProps: (item) => {
+                if (item.title && item.url) {
+                  return { label: `${item.title} (${item.url})` };
+                } else if (item.title && !item.url) {
+                  return { label: `${item.title}` };
+                } else if (!item.title && item.url) {
+                  return { label: `${item.url}` };
+                } else {
+                  return { label: "Link" };
+                }
+              },
+            },
+            fields: [
+              {
+                type: "string",
+                name: "title",
+                label: "Title",
+              },
+              {
+                type: "string",
+                name: "url",
+                label: "URL",
+              },
+            ],
+          },
+          {
+            type: "string",
+            name: "forks",
+            label: "Forks of this extension",
+            list: true,
+          },
+          {
+            type: "string",
+            name: "deprecated",
+            label: "Deprecating extensions",
+            list: true,
+          },
+          {
+            label: "Beta status",
+            name: "betaStatus",
+            type: "object",
+            fields: [
+              {
+                type: "boolean",
+                name: "inBeta",
+                label: "In beta",
+              },
+              {
+                type: "string",
+                name: "since",
+                label: "Since",
+              },
+              {
+                type: "string",
+                name: "reason",
+                label: "Reason",
+                ui: {
+                  component: "textarea",
+                },
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: "tools",
+        label: "Tools",
+        path: "content/tools",
+        fields: [
+          {
+            type: "string",
+            name: "url",
+            label: "Tool URL",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "boolean",
+            name: "isNotWebsite",
+            label: "Is not a website",
+          },
+          {
+            type: "rich-text",
+            name: "description",
+            label: "Description",
+            required: true,
+            isBody: true,
+          },
+          {
+            label: "Links",
+            name: "links",
+            type: "object",
+            list: true,
+            ui: {
+              itemProps: (item) => {
+                if (item.title && item.url) {
+                  return { label: `${item.title} (${item.url})` };
+                } else if (item.title && !item.url) {
+                  return { label: `${item.title}` };
+                } else if (!item.title && item.url) {
+                  return { label: `${item.url}` };
+                } else {
+                  return { label: "Link" };
+                }
+              },
+            },
+            fields: [
+              {
+                type: "string",
+                name: "title",
+                label: "Title",
+              },
+              {
+                type: "string",
+                name: "url",
+                label: "URL",
+              },
+            ],
+          },
+          {
+            type: "string",
+            name: "forks",
+            label: "Forks of this extension",
+            list: true,
+          },
+          {
+            type: "string",
+            name: "deprecated",
+            label: "Deprecating extensions",
+            list: true,
+          },
+          {
+            label: "Beta status",
+            name: "betaStatus",
+            type: "object",
+            fields: [
+              {
+                type: "boolean",
+                name: "inBeta",
+                label: "In beta",
+              },
+              {
+                type: "string",
+                name: "since",
+                label: "Since",
+              },
+              {
+                type: "string",
+                name: "reason",
+                label: "Reason",
+                ui: {
+                  component: "textarea",
+                },
+              },
+            ],
+          },
+        ],
+      },
+      {
         name: "authors",
         label: "Authors",
         path: "content/authors",
