af013476068e7c47f3730c80cdbe8e639c0af86d
Cyrus Yiu <UnsignedArduino@outlook.com>
2024-07-12 18:41:32 -0400

    Pictures for cards on home page

--- src/components/QuickLinks/types.tsx
+++ src/components/QuickLinks/types.tsx
@@ -1,6 +1,13 @@
+import { StaticImageData } from "next/image";
+
 export type QuickLink = {
   name: string;
   description: string;
   link: string;
   linkText: string;
+  image?: {
+    darkTheme: StaticImageData;
+    lightTheme: StaticImageData;
+    altText: string;
+  };
 };
