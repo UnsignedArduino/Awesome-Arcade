ddd368f9e5c387f3f722c999ef62092f972dab1a
Cyrus Yiu <UnsignedArduino@outlook.com>
2024-07-13 18:16:52 -0400

    Track user preferred layout
    Fixes https://github.com/UnsignedArduino/Awesome-Arcade/issues/166

--- src/components/Analytics/specific.ts
+++ src/components/Analytics/specific.ts
@@ -1,3 +1,5 @@
+import { ListLayout } from "@/components/AwesomeArcadeList/listLayout";
+
 export namespace AnalyticEvents {
   export function sendAwesomeClick(type: "extension" | "tool", repo: string) {
     window.gtag("event", "click_awesome", {
@@ -36,7 +38,12 @@ export namespace AnalyticEvents {
     theme: "light" | "dark" | "auto light" | "auto dark",
   ) {
     // https://ithoughthecamewithyou.com/post/user-scoped-custom-dimensions-in-google-analytics-4-using-gtag
-    // console.log(`Setting user preferred theme to ${theme}`);
+    console.log(`Setting user preferred theme to ${theme}`);
     window.gtag("set", "user_properties", { theme: theme });
+  }
+
+  export function setUserPreferredLayout(layout: ListLayout) {
+    console.log(`Setting user preferred layout to ${layout}`);
+    window.gtag("set", "user_properties", { layout: layout });
   }
 }
